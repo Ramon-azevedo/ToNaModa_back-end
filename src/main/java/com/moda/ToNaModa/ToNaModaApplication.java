8f4c0be755c4ae1cce777865cda226a3a7ec7a8e
Francisco Ramone <131681889+Ramon-azevedo@users.noreply.github.com>
2024-11-21 22:16:06 -0300

    1-commit

---
+++ src/main/java/com/moda/ToNaModa/ToNaModaApplication.java
@@ -0,0 +1,13 @@
+package com.moda.ToNaModa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToNaModaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToNaModaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moda/ToNaModa/ToNaModaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moda.ToNaModa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToNaModaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
