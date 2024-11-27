9043d716b6bce4725dbd972249f92f2ef329a703
Francisco Ramone <131681889+Ramon-azevedo@users.noreply.github.com>
2024-11-27 00:40:12 -0300

    new

--- src/main/java/com/moda/ToNaModa/repositorios/FemininoRepositorio.java
+++ src/main/java/com/moda/ToNaModa/repositorios/FemininoRepositorio.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.moda.ToNaModa.identidades.Feminino;
 
-public interface FemininoRepositorio extends JpaRepository<Long,Feminino> {
+public interface FemininoRepositorio extends JpaRepository<Feminino,Long> {
 
 }
